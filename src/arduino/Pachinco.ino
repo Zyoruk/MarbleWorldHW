2c0b03e7de8b706727066b1f847d5e1b9767ddf5
jeukel <jeukel@gmail.com>
2015-06-20 23:22:19 -0600

    Pachinco controls DONE.

--- src/arduino/Pachinco.ino
+++ src/arduino/Pachinco.ino
@@ -5,24 +5,25 @@ Servo servo01;  // create servo object to control a servo
 Servo servo02;  // create servo object to control a servo 
 
 //Input
-String servoNum = "";   // a string to hold incoming data
+char c = 'A';
 String time = "";         // a string to hold incoming data
 long timetoMove = -1;
 boolean stringComplete = false;  // whether the string is complete
 
 //State
 int state = 0;
-String curr1 = "X";
-String curr2 = "ACE";
+char curr1 = 'X';
+char curr2 = 'A';
 int movSix = 90;
 int mov90 = 148;
-String c;
+String s;
 
 void setup() {
   Serial.begin(9600);
-  servo01.attach(5);  // attaches the servo on pin 9 to the servo object 
-  servo01.attach(3);  // attaches the servo on pin 9 to the servo object
-  servoNum.reserve(10); 
+  servo01.attach(9);  // attaches the servo on pin 9 to the servo object 
+  servo02.attach(10);  // attaches the servo on pin 9 to the servo object
+  time.reserve(10);
+   
   //pinMode(LED, OUTPUT);    //tell Arduino LED is an output
   //pinMode(BUTTON, INPUT);  //BUTTON is an input
   //pinMode(speakerPin, OUTPUT);    //and BUZ is an output
@@ -30,189 +31,156 @@ void setup() {
 
 void loop(){
   if (stringComplete) {
-    Serial.println(servoNum);
+    Serial.println(c);
     Serial.println(time);
-    if(servoNum == "A"){
-      servoNum = "ACE";
-    }
+
+    switch(c){
+      case 'C':
+        c = 'A';
+        break;
+      case 'D':
+        c = 'B';
+        break;
+      case 'E':
+        c = 'A';
+        break;
+      case 'F':
+        c = 'B';
+        break;
+    }    
+     
+    if( timetoMove == 0){
     
-    if(servoNum == "C"){
-      servoNum = "ACE";
-    }
-    
-    if(servoNum == "E"){
-      servoNum = "ACE";
-    }
-    
-    if(servoNum == "B"){
-      servoNum = "BDF";      
-    }
-    
-    if(servoNum == "D"){
-      servoNum = "BDF";      
-    }
-    
-    if(servoNum == "F"){
-      servoNum = "BDF";      
-    }
-    
-    Serial.println(servoNum);
-    
-    if( timetoMove == 0){  
-      
-      if( servoNum == "X" || "Y" || "Z"){ //CHANGE
+      switch(c){
         
-        if(curr1 == "Y" ){
-          if(servoNum == "X"){
-            curr1 = "X";
-            servo01.write(-1 * mov90);
-            delay(700);
+        case 'A':
+          if(curr2 == 'B' ){
+              curr2 = 'A';
+              servo02.write(movSix); 
+              delay(700);
           }
-          
-          if(servoNum == "Z"){
-            curr1 = "Z";
-            servo01.write(mov90); 
-            delay(700);
-          }  
-        }
+          break;
+        case 'B':
+          if(curr2 == 'A' ){
+              curr2 = 'B';
+              servo02.write(-1 * movSix); 
+              delay(700);
+          }          
+          break;
         
-        if(curr1 == "X"){
-          if(servoNum == "Y"){
-            curr1 = "Y";
-            servo01.write(mov90);
-            delay(700);
+        case 'X':
+          if(curr1 == 'Y' ){
+              curr1 = 'X';
+              servo01.write(-1 * mov90); 
+              delay(700);
           }
-          
-          if(servoNum == "Z"){
-            curr1 = "Z";
-            servo01.write(mov90);
-            delay(700);
-            servo01.write(mov90);
-            delay(700);
-          }  
-        }
-        
-        if(curr1 == "Z"){
-          if(servoNum == "Y"){
-            curr1 = "Y";
-            servo01.write(-1 * mov90);
-            delay(700);          
+          if(curr1 == 'Z' ){
+              curr1 = 'X';
+              servo01.write(-1 * mov90);
+              delay(700);
           }
-          
-          if(servoNum == "X"){
-            curr1 = "X";
-            servo01.write(-1 * mov90);
-            delay(700);
-            servo01.write(-1 * mov90); 
-            delay(700);
+          break;
+        case 'Y':
+          if(curr1 == 'Z' ){
+              curr1 = 'Y';
+              servo01.write(-1 * mov90); 
+              delay(700);
           } 
-        }
-            
-      }else{     
-        if(curr2 == "ACE" ){
-          if(servoNum == "BDF"){
-            Serial.println("HERE PLEASE");
-            curr2 = "BDF";
-            Serial.println(movSix);
-            servo02.write(movSix); 
-            delay(700);
+          if(curr1 == 'X' ){
+              curr1 = 'Y';
+              servo01.write(mov90); 
+              delay(700);
+          }         
+          break;
+        case 'Z':
+          if(curr1 == 'Y' ){
+              curr1 = 'Z';
+              servo01.write(-1 * mov90); 
+              delay(700);
           }
-        }
-        if(curr2 == "BDF" ){
-          if(servoNum == "ACE"){
-            curr2 = "ACE";
-            servo02.write(-1 * movSix); 
-            delay(700);
+          if(curr1 == 'Y' ){
+              curr1 = 'Z';
+              servo01.write(-1 * mov90); 
+              delay(700);
           }
-        }
-      }  
+          break;
+      }
+              
     }else{
-      if( servoNum == "X" || "Y" || "Z" ){
+      switch(c){
         
-        if(curr1 == "Y"){
-          if(servoNum == "X"){
-            curr1 = "X";
-            servo01.write(-1 * mov90);
-            delay(700+timetoMove);
-            servo01.write(mov90);
-            delay(700);
+        case 'A':
+          if(curr2 == 'B' ){
+              curr2 = 'A';
+              servo02.write(-1 * movSix); 
+              delay(700+timetoMove);
+              servo02.write(movSix); 
+              delay(700);
           }
-
-          if(servoNum == "Z"){
-            curr1 = "Z";
-            servo01.write(mov90);
-            delay(700+timetoMove);
-            servo01.write(-1 * mov90); 
-            delay(700);
+          break;
+        case 'B':
+          if(curr2 == 'A' ){
+              curr2 = 'B';
+              servo02.write(movSix);
+              delay(700+timetoMove);
+              servo02.write(-1 * movSix); 
+              delay(700);
           }          
-        }
-  
-        if(curr1 == "X" ){
-          if(servoNum == "Y"){
-            curr1 = "Y";
-            servo01.write(mov90);
-            delay(700+timetoMove);
-            servo01.write(-1 * mov90);
-            delay(700);          
+          break;
+        
+        case 'X':
+          if(curr1 == 'Y' ){
+              curr1 = 'X';
+              servo01.write(-1 * movSix);
+              delay(700+timetoMove);
+              servo01.write(movSix); 
+              delay(700);
           }
-          
-          if(servoNum == "Z"){
-            curr1 = "Z";
-            servo01.write(mov90);
-            delay(700);
-            servo01.write(mov90);
-            delay(700+timetoMove);
-            servo01.write(-1 * mov90);
-            delay(700);
-            servo01.write(-1 * mov90); 
-            delay(700);
+          if(curr1 == 'Z' ){
+              curr1 = 'X';
+              servo01.write(-1 * movSix);
+              delay(700+timetoMove);
+              servo01.write(movSix); 
+              delay(700);
           }
-        }
-       
-        if(curr1 == "Z"){
-          if(servoNum == "Y"){
-            curr1 = "Y";
-            servo01.write(-1 * mov90);
-            delay(700+timetoMove);
-            servo01.write(mov90);
-            delay(700);
+          break;
+        case 'Y':
+          if(curr1 == 'Z' ){
+              curr1 = 'Y';
+              servo01.write(-1 * movSix);
+              delay(700+timetoMove);
+              servo01.write(movSix); 
+              delay(700);
+          } 
+          if(curr1 == 'X' ){
+              curr1 = 'Y';
+              servo01.write(movSix);
+              delay(700+timetoMove);
+              servo01.write(-1 * movSix); 
+              delay(700);
+          }         
+          break;
+        case 'Z':
+          if(curr1 == 'Y' ){
+              curr1 = 'Z';
+              servo01.write(-1 * movSix);
+              delay(700+timetoMove);
+              servo01.write(movSix);
+              delay(700);
           }
-
-          if(servoNum == "X"){
-            curr1 = "X";
-            servo01.write(-1 * mov90);
-            delay(700);
-            servo01.write(-1 * mov90);
-            delay(700+timetoMove);
-            servo01.write(mov90);
-            delay(700);
-            servo01.write(mov90);
-            delay(700);          
-          }          
-        }
-           
-      }else{
-        if(curr2 == "ACE"){
-          if(servoNum == "BDF"){
-            curr2 = "BDF";
-            servo02.write(movSix);
-            delay(700+timetoMove);
-            servo02.write(-1 * movSix ); 
-            delay(700);
+          if(curr1 == 'Y' ){
+              curr1 = 'Z';
+              servo01.write(-1 * movSix); 
+              delay(700+timetoMove);
+              servo01.write(movSix); 
+              delay(700);
           }
-        }
-        if(curr2 == "BDF"){
-          if(servoNum == "ACE"){
-            curr2 = "ACE";
-            servo02.write(-1 * movSix);
-            delay(700+timetoMove);
-            servo02.write(movSix); 
-            delay(700);
-          }          
-        }
+          break;
       }
     }
-  stringComplete = false;    
+    stringComplete = false;
+    state = 0;
+    time = "";
   }
 }
 
@@ -220,19 +188,21 @@ void serialEvent() {
   while (Serial.available()) {
     // get the new byte:
     char inChar = (char)Serial.read();
-    c = String(inChar);
-    if(c == ","){
+    s = String(inChar);
+    if(s == ","){
         state = 1;
     }else{
       if(state == 0){
-        servoNum += inChar;
+        c = inChar;
       }else{
         time += inChar;
       }
       if (inChar == '\n') {
         timetoMove = time.toInt();
         stringComplete = true;
+        s = "";
       }
     }     
   }
 }
+
